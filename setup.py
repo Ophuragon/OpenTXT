d0671586a375fc7f4e8ec2882de18db869b25487
uuphoria <123761856+uuphoria@users.noreply.github.com>
2023-12-03 11:57:27 -0600

    the tru final commit

---
+++ setup.py
@@ -0,0 +1,3 @@
+print("The first step If it doesnt work Please get python due to it being built with python, This step can be skipped if python is already installed")
+print("Dont have python Get it here!")
+print("https://www.python.org/downloads/")
