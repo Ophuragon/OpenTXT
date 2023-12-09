9d0132937fd99303152a70f170db34ac2e1926c9
uuphoria <123761856+uuphoria@users.noreply.github.com>
2023-12-08 21:02:11 -0600

    Delete setup.py

--- setup.py
+++
@@ -1,3 +0,0 @@
-print("The first step If it doesnt work Please get python due to it being built with python, This step can be skipped if python is already installed")
-print("Dont have python? Get it here!")
-print("https://www.python.org/downloads/")
